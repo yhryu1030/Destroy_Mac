d3767bd707430b652a054c46de0d0542e7f10bb2
HowardCai1 <hcai@macalester.edu>
2020-03-10 16:07:18 -0500

    make a stealth scene

--- src/game.js
+++ src/game.js
@@ -1,3 +1,79 @@
+var player;
+var platforms;
+var cursors;
+
+var stealth = new Phaser.Class({
+    Extends: Phaser.Scene,
+
+    initialize:
+
+    function sceneA ()
+    {
+        Phaser.Scene.call(this, { key: 'stealth' });
+    },
+
+    preload: function ()
+    {
+        this.load.image('obstacle', 'assets/obstacle.png');
+        this.load.image('student', 'assets/student.png');
+    },
+
+    create: function ()
+    {
+        platforms = this.physics.add.staticGroup();
+        platforms.create(400, 300, 'obstacle');
+        
+        this.add.image(400, 300, 'obstacle');
+    
+        // this.add.image(100,200,'student');
+        player = this.physics.add.sprite(100, 450, 'student');
+        player.setBounce(0);
+        player.setCollideWorldBounds(true);
+        cursors = this.input.keyboard.createCursorKeys();
+    
+        this.physics.add.collider(player, platforms);
+
+        // this.input.once('pointerdown', function () {
+
+        //     console.log('From SceneA to SceneB');
+
+        //     this.scene.start('sceneB');
+
+        // }, this);
+    },
+
+    update: function()
+    {
+        if (!cursors.up.isDown && !cursors.down.isDown){
+            player.setVelocityY(0);
+        }
+        if (!cursors.left.isDown && !cursors.right.isDown){
+            player.setVelocityX(0);    
+        }
+    
+        if (cursors.up.isDown)
+        {
+            player.setVelocityY(-160);
+        }
+        if (cursors.down.isDown)
+        {
+            player.setVelocityY(160);
+        }
+        if(cursors.right.isDown)
+        {
+            player.setVelocityX(160);
+        }
+        if (cursors.left.isDown)
+        {
+            player.setVelocityX(-160);
+        }
+
+    }
+    
+    
+
+});
+
 
 var config = {
     type: Phaser.AUTO,
@@ -10,70 +86,14 @@ var config = {
             debug: false
         }
     },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+    scene: [stealth]
 };
 
-var player;
-// var platforms;
-var cursors;
+
 
 var game = new Phaser.Game(config);
 
 
-function preload ()
-{
-    this.load.image('obstacle', 'assets/obstacle.png');
-    this.load.image('student', 'assets/student.png');
 
-    
-    
 
-}
-// var platforms;
-function create ()
-{
-    platforms = this.physics.add.staticGroup();
-    platforms.create(400, 300, 'obstacle');
-    
-    this.add.image(400, 300, 'obstacle');
 
-    // this.add.image(100,200,'student');
-    player = this.physics.add.sprite(100, 450, 'student');
-    player.setBounce(0);
-    player.setCollideWorldBounds(true);
-    cursors = this.input.keyboard.createCursorKeys();
-
-    this.physics.add.collider(player, platforms);
-}
-
-function update ()
-{   
-
-    if (!cursors.up.isDown && !cursors.down.isDown){
-        player.setVelocityY(0);
-    }
-    if (!cursors.left.isDown && !cursors.right.isDown){
-        player.setVelocityX(0);    
-    }
-
-    if (cursors.up.isDown)
-    {
-        player.setVelocityY(-160);
-    }
-    if (cursors.down.isDown)
-    {
-        player.setVelocityY(160);
-    }
-    if(cursors.right.isDown)
-    {
-        player.setVelocityX(160);
-    }
-    if (cursors.left.isDown)
-    {
-        player.setVelocityX(-160);
-    }
-}
