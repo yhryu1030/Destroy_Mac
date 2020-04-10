cc2dc39b24f644cda4294cf96aae34c038fd1c53
Niketh27 <31743194+Niketh27@users.noreply.github.com>
2020-04-10 18:49:29 -0500

    sound in frenzy
    added the basic punching sound effect

--- src/frenzy.js
+++ src/frenzy.js
@@ -17,12 +17,16 @@ export default new Phaser.Class({
     {
         // this.load.image('imac', 'assets/imac.jpg'); // from https://www.pexels.com/photo/photo-of-imac-near-macbook-1029757/
         this.load.multiatlas('punching', 'assets/punching.json', 'assets'); // loads sprite sheet
+        this.load.audio('punchSound','assets/sound/punch1.mp3'); //loads the punching sound
     },
 
     create: function ()
     {
         // background
         var background = this.add.sprite(0, 0, 'punching', 'imac.jpg');
+
+        //Sound
+        this.punch1 = this.sound.add('punchSound');
 
         // sprite
         fist = this.add.sprite(400, 600, 'punching', 'punch/punch1.png');
@@ -32,6 +36,9 @@ export default new Phaser.Class({
         var frameNames = this.anims.generateFrameNames('punching', { start: 1, end: 6, zeroPad: 1, prefix:'punch/punch', suffix:'.png' }); // calls all the images
         this.anims.create({ key: 'punch', frames: frameNames, frameRate: 30, repeat: -1 }); // sets speed and repetition of the animation
         fist.anims.play('punch');
+
+        //play Sound : test
+        //this.punch1.play();
 
         // this.add.sprite(400, 300, 'imac');
 
