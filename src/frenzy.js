495275bd4aa173ee722f40915ee543208ddeded9
Sun Gyu Park <31524410+samfan0429@users.noreply.github.com>
2020-05-04 18:23:10 -0500

    Made conditions and fixes that would change the total time
    Made conditions that would change the time limit in the frenzy mode. Made fixes where the time would appear as "NaN."

--- src/frenzy.js
+++ src/frenzy.js
@@ -45,8 +45,17 @@ export default new Phaser.Class({
 
     create: function ()
     {
-
+        //Changing time depending on the distance.
         console.log('The distance from the guard is :', this.distance);
+        if(this.distance>=300){
+            this.totalTime=10;
+        }
+        else if(this.distance>=150){
+            this.totalTime=6;
+        }
+        else{
+            this.totalTime=4;
+        }
 
         // background
         this.background = this.add.sprite(540, 310, 'imac');
@@ -116,9 +125,8 @@ export default new Phaser.Class({
         }, this);
 
         //Timer
-        this.timedEvent = this.time.delayedCall(10000, this.getCaught, [], this);
+        this.timedEvent = this.time.delayedCall(this.totalTime*1000, this.getCaught, [], this);
         this.timeText = this.add.text(16, 16, 'Time Left: ', { fontSize: '32px', fill: '#000' });
-        this.totalTime=10;
 
     },
 
